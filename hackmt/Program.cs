7d1363bf4a31bf4d3ac963f6bd7af3389959c099
j9cks <jacksonreed478@gmail.com>
2026-01-31 01:58:22 -0600

    umm

--- hackmt/Program.cs
+++ hackmt/Program.cs
@@ -15,9 +15,22 @@ class Program
         var conn = new MySqlConnection(connStr);
         conn.Open();
 
-        var cmd = new MySqlCommand("SELECT * FROM player;", conn);
+        var cmd = new MySqlCommand(@"SELECT g.gameID, p1.fName AS pitcherFirst,
+    p1.lName AS pitcherLast,
+    p2.fName AS batterFirst,
+    p2.lName AS batterLast,
+    pi.pitchType,
+    pi.outcome
+FROM pitch pi
+JOIN game g ON pi.gameID = g.gameID
+JOIN player p1 ON pi.pitcherID = p1.playerID
+JOIN player p2 ON pi.batterID = p2.playerID
+ORDER BY g.gameID, pi.pitchNumber;", conn);
         var reader = cmd.ExecuteReader();
 
+        /// The reader stores all data of the query with the fields included.
+        /// .Read() will give you one data row at a time.
+        /// Access attribute name with reader.GetName(i) and actual data with reader[i].
         while (reader.Read())
         {
             for (int i = 0; i < reader.FieldCount; i++)
